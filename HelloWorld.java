f8338ce71e4ad84d9696eea77f1455eae8ac2c3a
Samratdok <samratkotresh@gmail.com>
2021-09-25 16:46:04 +0530

    pushed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Lalitha (Delhi) and Samrat (bangalore) here now!");
+      System.out.println("Hello all, Lalitha (Delhi) and Samrat (bangalore) here now yes now!");
    }
 }
